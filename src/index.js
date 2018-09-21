20a6d7c29ecf2c9ac7994096de525b157b7f15fd
Tserekh Ivan <tserekh.ivan@gmail.com>
2018-09-21 03:58:21 +0300

    21 Sep 2018

--- src/index.js
+++ src/index.js
@@ -1,26 +1,91 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.value = [];
+    this.value[0] = initialValue;
   }
 
   add(number) {
-    // your implementation
+    let l = this.value.length;
+    this.value[l] = "+";
+    this.value[l+1] = number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    let l = this.value.length;
+    this.value[l] = "-";
+    this.value[l+1] = number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    let l = this.value.length;
+    this.value[l] = "*";
+    this.value[l+1] = number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    let l = this.value.length;
+    this.value[l] = "/";
+    this.value[l+1] = number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    let l = this.value.length;
+    this.value[l] = "^";
+    this.value[l+1] = number;
+    return this;
+  }
+
+  calc() {
+    let l = this.value.length;
+    let tmp;
+
+    if (l === 1) {
+      return this.value[0];
+    }
+      
+    for (let i = l - 2; i >=1; i = i - 2) {
+      if (this.value[i] === "^") {
+        tmp = this.value[i-1] ** this.value[i+1];
+        this.value.splice(i-1, 3, tmp);
+        
+      }
+    }
+
+    this.value = this.value.reverse();
+
+    l = this.value.length;
+    for (let i = l - 2; i >=1; i = i - 2) {
+      if (this.value[i] === "*") {
+        tmp = this.value[i-1] * this.value[i+1];
+        this.value.splice(i-1, 3, tmp);
+      }
+      if (this.value[i] === "/") {
+        tmp = this.value[i+1] / this.value[i-1];
+        this.value.splice(i-1, 3, tmp);
+      }
+    }
+
+    l = this.value.length;
+    for (let i = l - 2; i >=1; i = i - 2) {
+      if (this.value[i] === '+') {
+
+        tmp = this.value[i-1] + this.value[i+1];
+        this.value.splice(i-1, 3, tmp);
+      }
+      if (this.value[i] == '-') {
+        tmp = this.value[i+1] - this.value[i-1];
+        this.value.splice(i-1, 3, tmp);
+      }
+    }
+  }
+
+  toString() {
+    this.calc();
+    return this.value[0];
   }
 }
 
